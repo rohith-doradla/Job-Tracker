4e36560879ab607662a51df8ad90930708454575
Rohith <doradla.rohith@gmail.com>
2026-05-25 09:41:20 +0530

    auth2-2

---
+++ src/routes/index.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import HomePage from '../pages/HomePage.jsx';
+import RegisterPage from '../pages/RegisterPage.jsx';
+import LoginPage from '../pages/LoginPage.jsx';
+import ForgotPassword from '../pages/ForgotPassword.jsx';
+import ResetPassword from '../pages/ResetPassword.jsx';
+import MainLayout from '../pages/MainLayout/MainLayout.jsx';
+import DashboardPage from '../pages/DashboardPage.jsx';
+import BoardsPage from '../pages/BoardsPage.jsx';
+import JobsPage from '../pages/JobsPage.jsx';
+import ProfilePage from '../pages/ProfilePage.jsx';
+import ErrorPage from '../pages/ErrorPage.jsx';
+
+export const router = createBrowserRouter([
+  { path: '/home', element: <HomePage /> },
+
+  { path: '/register', element: <RegisterPage /> },
+  { path: '/login', element: <LoginPage /> },
+  { path: '/forgot-password', element: <ForgotPassword /> },
+  { path: '/reset-password', element: <ResetPassword /> },
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { path: '/', element: <DashboardPage /> },
+      { path: 'boards', element: <BoardsPage /> },
+      { path: 'jobs', element: <JobsPage /> },
+      { path: 'profile', element: <ProfilePage /> },
+    ],
+  },
+
+  { path: '*', element: <ErrorPage /> },
+]);
